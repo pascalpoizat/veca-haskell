b432983118ede55e9cd94b9ddd9ba38b1d494d65
Pascal Poizat <pascal.poizat@lip6.fr>
2017-01-09 11:29:29 +0100

    add initial version of module Tree

---
+++ src/Tree.hs
@@ -0,0 +1,35 @@
+-- module for Trees
+-- (c) 2016 Pascal Poizat
+-- github: pascal.poizat
+--
+-- This module is useless if Data.Tree supports different types in nodes and in leaves.
+-- (this module is also used to learn Haskell by the way)
+module Tree
+where
+
+-- tree with values of type a in the leaves and values of type b in the nodes
+data Tree a b
+  = Leaf a
+  | Node b [Tree a b]
+  deriving (Show)
+
+-- checks for the validity of a tree
+isValid :: Tree a b -> Bool
+isValid (Leaf _)   = True
+isValid (Node _ t) = not (null t)
+
+-- apply transformation f to the values in the leaves of a tree
+treeleafmap :: (a -> a') -> Tree a b -> Tree a' b
+treeleafmap f (Leaf x)   = Leaf (f x)
+treeleafmap f (Node x t) = Node x (map (treeleafmap f) t)
+
+-- apply transformation f to the values in the nodes of a tree
+treenodemap :: (b -> b') -> Tree a b -> Tree a b'
+treenodemap f (Leaf x)   = Leaf x
+treenodemap f (Node x t) = Node (f x) (map (treenodemap f) t)
+
+-- apply transformation f1 to the values in the leaves of a tree
+-- and transformation f2 to the values in the nodes of the tree
+treemap :: (a -> a') -> (b -> b') -> Tree a b -> Tree a' b'
+treemap f1 f2 (Leaf x)   = Leaf (f1 x)
+treemap f1 f2 (Node x t) = Node (f2 x) (map (treemap f1 f2) t)
