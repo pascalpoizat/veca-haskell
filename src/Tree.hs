599719e738d2a186083126059c57e450c6cd8d5d
Pascal Poizat <pascal.poizat@lip6.fr>
2017-01-09 18:37:30 +0100

    clean up imports

--- src/Tree.hs
+++ src/Tree.hs
@@ -13,7 +13,7 @@ module Tree ( Tree(..)
             , treedepth)
 where
 
-import qualified Data.Set as Set
+import Data.Set as S hiding (null, map)
 
 -- tree with values of type a in the leaves and values of type b in the nodes
 data Tree a b
@@ -43,9 +43,9 @@ treemap f1 f2 (Leaf x)   = Leaf (f1 x)
 treemap f1 f2 (Node x t) = Node (f2 x) (map (treemap f1 f2) t)
 
 -- get the set of all leaves
-treeleaves :: Ord a => Tree a b -> Set.Set a
-treeleaves (Leaf x)   = Set.singleton x
-treeleaves (Node _ t) = Set.unions (map treeleaves t)
+treeleaves :: Ord a => Tree a b -> S.Set a
+treeleaves (Leaf x)   = singleton x
+treeleaves (Node _ t) = unions (map treeleaves t)
 
 -- get the depth of the tree
 treedepth :: Tree a b -> Int
