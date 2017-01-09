281416c1ec9279b128be8ec7e1b40ed6df07094e
Pascal Poizat <pascal.poizat@lip6.fr>
2017-01-09 14:13:39 +0100

    add functions to get the set of leaves and the depth of trees

--- src/Tree.hs
+++ src/Tree.hs
@@ -4,14 +4,22 @@
 --
 -- This module is useless if Data.Tree supports different types in nodes and in leaves.
 -- (this module is also used to learn Haskell by the way)
-module Tree
+module Tree ( Tree(..)
+            , isValid
+            , treeleafmap
+            , treenodemap
+            , treemap
+            , treeleaves
+            , treedepth)
 where
+
+import qualified Data.Set as Set
 
 -- tree with values of type a in the leaves and values of type b in the nodes
 data Tree a b
   = Leaf a
   | Node b [Tree a b]
-  deriving (Show)
+  deriving Show
 
 -- checks for the validity of a tree
 isValid :: Tree a b -> Bool
@@ -33,3 +41,13 @@ treenodemap f (Node x t) = Node (f x) (map (treenodemap f) t)
 treemap :: (a -> a') -> (b -> b') -> Tree a b -> Tree a' b'
 treemap f1 f2 (Leaf x)   = Leaf (f1 x)
 treemap f1 f2 (Node x t) = Node (f2 x) (map (treemap f1 f2) t)
+
+-- get the set of all leaves
+treeleaves :: Ord a => Tree a b -> Set.Set a
+treeleaves (Leaf x)   = Set.singleton x
+treeleaves (Node _ t) = Set.unions (map treeleaves t)
+
+-- get the depth of the tree
+treedepth :: Tree a b -> Int
+treedepth (Leaf x) = 1
+treedepth (Node _ t) = 1 + maximum (map treedepth t)
