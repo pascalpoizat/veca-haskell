0aee8f3923189592bf56ee2d8842d0875a12262f
pascalpoizat <pascal.poizat@lip6.fr>
2017-03-15 21:37:21 +0100

    feat: add a typeclass of communication events

---
+++ src/Models/Communication.hs
@@ -0,0 +1,52 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Models.Communication
+-- Copyright   :  (c) 2017 Pascal Poizat
+-- License     :  Apache-2.0 (see the file LICENSE)
+--
+-- Maintainer  :  pascal.poizat@lip6.fr
+-- Stability   :  experimental
+-- Portability :  unknown
+--
+-- A class for communication events.
+-----------------------------------------------------------------------------
+
+module Models.Communication (
+  Communication(..)
+  )
+where
+
+import Models.Complementary
+import Models.Internal
+
+{- |
+A typeclass for types (often events) with a complementary relation.
+
+Minimal definition: 'isInput' or 'isOutput'.
+Note that 'isInternal' and 'complementary' must also be defined
+due to the relation to 'Complementary' and 'Internal'.
+
+While defining your instances (for 'Complementary', 'Internal', and 'Communication') you should ensure:
+
+@
+forall x, 'isInput' x implies not ('isOutput' x) and not ('isInternal' x)
+
+forall x, 'isOutput' x implies not ('isInput' x) and not ('isInternal' x)
+
+forall x, 'isInternal' x implies not ('isOutput' x) and not ('isInput' x)
+
+forall x, either 'isInternal' x, 'isInput' x, or 'isOutput' x is true
+
+forall x, 'isInput' x implies 'isOutput' $ 'complementary' x
+
+forall x, 'isOutput' x implies 'isInput' $ 'complementary' x
+
+forall x, 'isInternal' x implies 'isInternal' $ 'complementary' x
+@
+-}
+class (Complementary p, Internal p) => Communication p where
+  {-# MINIMAL isInput | isOutput #-}
+  isInput :: p -> Bool
+  isOutput :: p -> Bool
+  isInput p = not (isOutput p || isInternal p)
+  isOutput p = not (isInput p || isInternal p)
